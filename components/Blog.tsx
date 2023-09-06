bf8e83ff0dde0bae85aef248695c7b4235c20df9
NaviTheCoderboi <102732282+NaviTheCoderboi@users.noreply.github.com>
2023-09-06 16:59:54 +0530

    fixed blog page

--- components/Blog.tsx
+++ components/Blog.tsx
@@ -33,18 +33,6 @@ const Blog = ({
         Record<string, unknown>
     >;
 }) => {
-    const getRandomBg = () => {
-        let colors = [
-            "bg-blue-500",
-            "bg-green-500",
-            "bg-yellow-500",
-            "bg-violet-500",
-            "bg-pink-500",
-        ];
-        let random = Math.floor(Math.random() * colors.length);
-        return colors[random];
-    };
-
     const handleScrollToHeading = (
         event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
     ) => {
@@ -138,7 +126,7 @@ const Blog = ({
                                     blog.frontmatter.tags.map((tag: string) => (
                                         <div
                                             key={tag}
-                                            className={`px-2 py-1 text-white rounded-full text-base font-medium ${getRandomBg()}`}
+                                            className="px-2 py-1 text-white rounded-full text-base font-medium bg-pink-500"
                                         >
                                             {tag}
                                         </div>
